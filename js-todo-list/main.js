acc313eec9f3e933db249aac1e8cd9cb827bc486
Noel Hsiao <huihsinhsiao12@gmail.com>
2021-04-20 17:04:31 +0800

    新增第三功能

--- js-todo-list/main.js
+++ js-todo-list/main.js
@@ -1,8 +1,11 @@
 document.addEventListener('DOMContentLoaded', function() {
+
+  //功能1
   document.querySelector('ul').addEventListener('click', function(e) {
     e.target.classList.toggle('checked');
   })
 
+  //功能2
   const cols = document.querySelectorAll('.close')
   cols.forEach(function(d) {
     d.addEventListener('click', function(d) {
@@ -10,4 +13,20 @@ document.addEventListener('DOMContentLoaded', function() {
       d.target.parentElement.remove();
     })
   })
+
+  //功能3
+  document.querySelector('#addBtn').addEventListener('click', function(e) {
+    // debugger
+    const inputValue = e.target.previousElementSibling.value;
+    const newUl = document.querySelector('ul');
+    const newLi = document.createElement('li');
+    newLi.innerHTML = inputValue + '<span class="close">x</span>'
+    newUl.appendChild(newLi);
+    e.target.previousElementSibling.value = '';
+    //重新綁上監聽
+    newLi.firstElementChild.addEventListener('click', function() {
+      newLi.remove();
+    })
+  })
+
 })
