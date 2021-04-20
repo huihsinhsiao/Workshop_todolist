98f53b5dc358853d320407df39d9349387145dea
Noel Hsiao <huihsinhsiao12@gmail.com>
2021-04-20 12:05:23 +0800

    nothing

--- js-todo-list/main.js
+++ js-todo-list/main.js
@@ -2,4 +2,8 @@ document.addEventListener('DOMContentLoaded', function() {
   document.querySelector('ul').addEventListener('click', function(e) {
     e.target.classList.toggle('checked');
   })
+
+  document.querySelector('.close').addEventListener('click', function(e) {
+    e.target.
+  })
 })
